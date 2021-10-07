118371d41ff9b72c967af6a5063e518f37cd5d14
Jason Moore <87956172+jasonsmoore@users.noreply.github.com>
2021-10-07 14:15:10 -0700

    final tdd-lab-2

--- tests/Cart.test.js
+++ tests/Cart.test.js
@@ -1,3 +1,96 @@
+const Cart = require('../src/js/Cart')
+const Product = require('../src/js/Product')
 describe("Cart", () => {
-  test.todo("add Cart tests here");
+  test('items set to empty array', () => {
+    const cart = new Cart();
+    expect(cart.items).toEqual([]);
+  });
+  test('make sure product adds to array Candy', () => {
+    const cart = new Cart();
+    const product = new Product('Candy', 1.75, true);
+    cart.add(product);
+    expect(cart.items).toEqual(expect.arrayContaining([expect.objectContaining({name: 'Candy', price: 1.75, taxable: true})]));
+  });
+  test('make sure product adds to array Candy/Pop', () => {
+    const cart = new Cart();
+    const candy = new Product('Candy', 1.75, true);
+    const pop = new Product('Pop', 2.50, true);
+    cart.add(candy);
+    cart.add(pop);
+    expect(cart.items).toEqual(expect.arrayContaining([expect.objectContaining({name: 'Candy', price: 1.75, taxable: true})]));
+  });
+  test('get total item count of array to be 2', () => {
+    const cart = new Cart();
+    const candy = new Product('Candy', 1.75, true);
+    const pop = new Product('Pop', 2.50, true);
+    cart.add(candy);
+    cart.add(pop);
+    expect(cart.getItemCount()).toEqual(2);
+  });
+  test('get total item count of array to be 3', () => {
+    const cart = new Cart();
+    const candy = new Product('Candy', 1.75, true);
+    const pop = new Product('Pop', 2.50, true);
+    const canned = new Product('Canned Goods', 3.15, false);
+    cart.add(candy);
+    cart.add(pop);
+    cart.add(canned);
+    expect(cart.getItemCount()).toEqual(3);
+  });
+  test('total price of all items', () => {
+    const cart = new Cart();
+    const candy = new Product('Candy', 1.75, true);
+    const pop = new Product('Pop', 2.50, true);
+    const canned = new Product('Canned Goods', 3.15, false);
+    cart.add(candy);
+    cart.add(pop);
+    cart.add(canned);
+    expect(cart.getTotalBeforeTax()).toBeCloseTo(7.40);
+  });
+  test('total price of all items', () => {
+    const cart = new Cart();
+    const cheese = new Product('Cheese', 4.55, false);
+    const milk = new Product('Milk', 5.50, false);
+    const toiletPaper = new Product('Toilet Paper', 8.95, true);
+    const light = new Product('Light Bulbs', 11.49, true);
+    cart.add(cheese);
+    cart.add(milk);
+    cart.add(toiletPaper);
+    cart.add(light);
+    expect(cart.getTotalBeforeTax()).toBeCloseTo(30.49);
+  });
+  test('total price of all items with tax', () => {
+    const cart = new Cart();
+    const cheese = new Product('Cheese', 4.55, false);
+    const milk = new Product('Milk', 5.50, false);
+    const toiletPaper = new Product('Toilet Paper', 8.95, true);
+    const light = new Product('Light Bulbs', 11.49, true);
+    cart.add(cheese);
+    cart.add(milk);
+    cart.add(toiletPaper);
+    cart.add(light);
+    expect(cart.getTotalWithTax()).toBeCloseTo(32.53);
+  });
+  test('total price of all items with tax', () => {
+    const cart = new Cart();
+    const candy = new Product('Candy', 1.75, true);
+    const pop = new Product('Pop', 2.50, true);
+    const canned = new Product('Canned Goods', 3.15, false);
+    cart.add(candy);
+    cart.add(pop);
+    cart.add(canned);
+    expect(cart.getTotalWithTax()).toBeCloseTo(7.83);
+  });
+  test('get price of just the tax', () => {
+    const cart = new Cart();
+    const cheese = new Product('Cheese', 4.55, false);
+    const milk = new Product('Milk', 5.50, false);
+    const toiletPaper = new Product('Toilet Paper', 8.95, true);
+    const light = new Product('Light Bulbs', 11.49, true);
+    cart.add(cheese);
+    cart.add(milk);
+    cart.add(toiletPaper);
+    cart.add(light);
+    expect(cart.getTax()).toBeCloseTo(2.04);
+  });
 });
--- tests/Product.test.js
+++ tests/Product.test.js
@@ -1,3 +1,21 @@
+const Product = require('../src/js/Product');
 describe("Product", () => {
-  test.todo("add Product tests here");
+    test('Should contain properties name, price, taxable', () => {
+      const product = new Product('Chips', 1.50, true);
+      expect(product).toHaveProperty('name');
+      expect(product).toHaveProperty('price');
+      expect(product).toHaveProperty('taxable');
+    });
+    test('If price is taxable, returns price + 10% tax', () => {
+      const product = new Product('Candy', 1.75, true);
+      expect(product.getPriceWithTax()).toBeCloseTo(1.925);
+    });
+    test('If price is taxable, returns price + 10% tax', () => {
+      const product = new Product('Pop', 2.50, true);
+      expect(product.getPriceWithTax()).toBeCloseTo(2.75);
+    });
+    test('If price is not taxable, returns price', () => {
+      const product = new Product('Canned Goods', 3.15, false);
+      expect(product.getPriceWithTax()).toBeCloseTo(3.15);
+    });
 });
